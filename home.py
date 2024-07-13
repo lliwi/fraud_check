4cb796ce9f0974b4a090eab9bd7e1462fa6380b6
lliwi <llibert.morreres@gmail.com>
2024-07-13 10:35:57 +0200

    auth set

--- home.py
+++ home.py
@@ -3,7 +3,6 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
-
 st.set_page_config(
     page_title="Homepage",
     page_icon="🏡",
@@ -11,11 +10,39 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+if all (key not in st.session_state.keys() for key in ('username', 'pwd', 'pwd_correct', 'form_submitted')):
+    st.session_state['username'] = ""
+    st.session_state['pwd'] = ""
+    st.session_state['pwd_correct'] = False
+    st.session_state['form_submitted'] = False
 
+def check_login():
+    st.session_state['form_submitted'] = True
 
-if __name__ == "__main__":
+    if (
+        st.session_state["username"] in st.secrets["passwords"]
+        and 
+        st.session_state["pwd"] == st.secrets["passwords"][st.session_state["username"]]
+    ):
+        st.session_state['pwd_correct'] = True
+        st.session_state['pwd'] = ""
+        st.session_state['username'] = ""
+    else:
+        st.session_state['pwd_correct'] = False
 
-    #st.title("Homepage")
+def display_login_form():
+    with st.form("login_form"):
+        st.text_input("Username", key="username")
+        st.text_input("Password", type="password", key="pwd")
+
+        st.form_submit_button("Login", on_click=check_login)
+
+if (st.session_state['pwd_correct'] == False and st.session_state['form_submitted'] == False):
+    display_login_form()
+elif (st.session_state['pwd_correct'] == False and st.session_state["form_submitted"] == True):
+    display_login_form()
+    st.error("Invalid user/password")
+elif (st.session_state['pwd_correct'] == True and st.session_state["form_submitted"] == True):
     st.image('./media/banner.png')
 
     if 'IPQS_API_KEY' not in st.session_state:
@@ -30,3 +57,7 @@ if __name__ == "__main__":
     
     with st.expander("Recursos OSINT"):
         st.dataframe(df,hide_index=True)
+else:
+    display_login_form()
+
+#st.write(st.session_state)
